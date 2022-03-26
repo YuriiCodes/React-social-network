f43bd9893c525f1217123168ace2dd38c3ea9dee
Yurii <yuriipidlisnyi2021@gmail.com>
2022-03-26 15:47:00 +0200

    refactored dispatch logic, moved from IFs to switch

--- src/data/state.js
+++ src/data/state.js
@@ -23,62 +23,69 @@ let store = {
     _callSubscriber() {
         console.log('State changed')
     },
-    dispatch(action) {
-        if (action.type === "ADD-POST") {
-            let newPost = {
-                id: 5,
-                text: this._state.profilePage.newPostText,
-                likes: 0
-            }
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = "";
-            this._reRenderEntireTree(this._state);
-        } else if (action.type === "UPDATE-NEW-POST-TEXT") {
-            this._state.profilePage.newPostText = (action.newPostText);
-            this._reRenderEntireTree(this._state);
-        } else if (action.type === "GET-STATE") {
-            return this._state;
-        } else if (action.type === "UPDATE-NEW-MESSAGE-TEXT") {
-            this._state.dialogsPage.newMessageText = action.newMessageText;
-            this._reRenderEntireTree(this._state);
-        } else if (action.type === "SEND-MESSAGE") {
-            let newMessage = {chatId: 1, message: [this._state.dialogsPage.newMessageText]};
-            this._state.dialogsPage.messages.push(newMessage);
-            this._state.dialogsPage.newMessageText = "";
-            this._reRenderEntireTree(this._state);
-        }
-    },
     getState() {
         return this._state;
     },
     subscribe(observerFn) {
         this._reRenderEntireTree = observerFn; //observer pattern
     },
-
-    addPost() {
-        let newPost = {
-            id: 5,
-            text: this._state.profilePage.newPostText,
-            likes: 0
+    dispatch(action) {
+        switch (action.type){
+            case "ADD-POST":
+                let newPost = {
+                    id: 5,
+                    text: this._state.profilePage.newPostText,
+                    likes: 0
+                }
+                this._state.profilePage.posts.push(newPost);
+                this._state.profilePage.newPostText = "";
+                this._reRenderEntireTree(this._state);
+                break;
+            case "UPDATE-NEW-POST-TEXT":
+                this._state.profilePage.newPostText = (action.newPostText);
+                this._reRenderEntireTree(this._state);
+                break;
+            case "GET-STATE":
+                return this._state;
+            case "UPDATE-NEW-MESSAGE-TEXT":
+                this._state.dialogsPage.newMessageText = action.newMessageText;
+                this._reRenderEntireTree(this._state);
+                break;
+            case "SEND-MESSAGE":
+                let newMessage = {chatId: 1, message: [this._state.dialogsPage.newMessageText]};
+                this._state.dialogsPage.messages.push(newMessage);
+                this._state.dialogsPage.newMessageText = "";
+                this._reRenderEntireTree(this._state);
+                break;
+            default:
+                console.log("You've entered wrong type in dispatch action. Please fix that!")
         }
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = "";
-        this._reRenderEntireTree(this._state);
     },
-    updateNewPostText(newPostText) {
-        this._state.profilePage.newPostText = (newPostText);
-        this._reRenderEntireTree(this._state);
-    },
-    updateNewMessageText(newMessageText) {
-        this._state.dialogsPage.newMessageText = newMessageText;
-        this._reRenderEntireTree(this._state);
-    },
-    sendMessage() {
-        let newMessage = {chatId: 1, message: [this._state.dialogsPage.newMessageText]};
-        this._state.dialogsPage.messages.push(newMessage);
-        this._state.dialogsPage.newMessageText = "";
-        this._reRenderEntireTree(this._state);
-    },
+
+    // addPost() {
+    //     let newPost = {
+    //         id: 5,
+    //         text: this._state.profilePage.newPostText,
+    //         likes: 0
+    //     }
+    //     this._state.profilePage.posts.push(newPost);
+    //     this._state.profilePage.newPostText = "";
+    //     this._reRenderEntireTree(this._state);
+    // },
+    // updateNewPostText(newPostText) {
+    //     this._state.profilePage.newPostText = (newPostText);
+    //     this._reRenderEntireTree(this._state);
+    // },
+    // updateNewMessageText(newMessageText) {
+    //     this._state.dialogsPage.newMessageText = newMessageText;
+    //     this._reRenderEntireTree(this._state);
+    // },
+    // sendMessage() {
+    //     let newMessage = {chatId: 1, message: [this._state.dialogsPage.newMessageText]};
+    //     this._state.dialogsPage.messages.push(newMessage);
+    //     this._state.dialogsPage.newMessageText = "";
+    //     this._reRenderEntireTree(this._state);
+    // },
 }
 window.store = store;
 export default store;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from "./data/state";
 
 let reRenderEntireTree = (state) =>{ ReactDOM.render(
     <React.StrictMode>
-        <App state = {store.getState()} dispatch={store.dispatch.bind(store)} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} sendMessage={store.sendMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}/>
+        <App state = {store.getState()} dispatch={store.dispatch.bind(store)} />
     </React.StrictMode>,
     document.getElementById('root')
 )};
